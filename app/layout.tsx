cafe5cd8a66e9b60509f07882a769c1e4337f3bc
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2023-01-28 15:23:36 -0500

    pass font variables thru props to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import "./globals.css";
+import { Amiko, Actor } from "@next/font/google";
+
+const amiko = Amiko({weight:"400", variable: "--display-font"});
+const actor = Actor({weight:"400", variable: "--body-font"});
 
 export default function RootLayout({
   children,
@@ -6,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${amiko.variable} ${actor.variable}`}>
       <head />
       <body>{children}</body>
     </html>
