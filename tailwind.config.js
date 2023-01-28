a97b15f4907a3821f26345eb480f02f3eec7f990
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2023-01-28 14:35:34 -0500

    Add custom colours to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,20 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'maxgreen': '#608b47',
+      'bud-green': '#89b153',
+      'android-green': '#a7c957',
+      'yellow-green': '#cdd993',
+      'eggshell': '#f2e8cf',
+      'pale-purple': '#e6d4e7',
+      'mauve': '#dabfff',
+      'lavender': '#b59deb',
+      'med-purple': '#907ad6',
+      'berry-purple': '#583abb',
+    },
   },
   plugins: [],
 };
